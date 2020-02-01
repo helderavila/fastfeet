8d8df9be29e097e96166f64b8d6e12b8bf75f0ee
Helder Avila <helderavila2016@gmail.com>
2020-02-01 12:33:41 -0300

    Validação para criação de um entregador

--- backend/src/app/controllers/DelivererController.js
+++ backend/src/app/controllers/DelivererController.js
@@ -1,8 +1,19 @@
+import * as Yup from 'yup';
 import Deliverer from '../models/Deliverer';
 import User from '../models/User';
 
 class DelivererController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().required(),
+      avatar_id: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation Fails' });
+    }
+
     const checkUserProvider = await User.findOne({
       where: { id: req.userId, provider: true },
     });
