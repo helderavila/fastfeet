0cae53dd43d127d3ce80116c2983c6a829fde904
Helder Avila <helderavila2016@gmail.com>
2020-02-04 10:07:13 -0300

    Envio de email para o entregador

--- backend/src/app/controllers/OrderController.js
+++ backend/src/app/controllers/OrderController.js
@@ -1,8 +1,32 @@
 import Order from '../models/Order';
+import Recipient from '../models/Recipient';
+import Deliverer from '../models/Deliverer';
+import Mail from '../../lib/Mail';
 
 class OrderController {
   async store(req, res) {
+    const { deliverer_id, product, recipient_id } = req.body;
+
+    const deliverer = await Deliverer.findByPk(deliverer_id);
+    const recipient = await Recipient.findByPk(recipient_id);
+
     const order = await Order.create(req.body);
+
+    await Mail.sendEmail({
+      to: `${deliverer.name} <${deliverer.email}>`,
+      subject: 'Uma nova encomenda disponivel!',
+      template: 'create',
+      context: {
+        deliverer: deliverer.name,
+        product,
+        name: recipient.name,
+        street: recipient.street,
+        number: recipient.number,
+        state: recipient.state,
+        city: recipient.city,
+        postcode: recipient.postcode,
+      },
+    });
 
     return res.json(order);
   }
