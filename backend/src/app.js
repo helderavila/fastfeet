a9c5ab86d1a8c165f173efc449e84f975f168ed7
Helder Avila <helderavila2016@gmail.com>
2020-02-04 17:21:04 -0300

    fix(images): images can be accessed in the browser

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
 import 'dotenv/config';
+import path from 'path';
 import express from 'express';
 import routes from './routes';
 
@@ -14,6 +15,10 @@ class App {
 
   middlewares() {
     this.server.use(express.json());
+    this.server.use(
+      '/signatures',
+      express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+    );
   }
 
   routes() {
